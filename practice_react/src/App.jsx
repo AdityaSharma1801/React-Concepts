192321f67a3d4948f02e5c3ecbb2efb01cdb545b
aditya sharma <sharmaaditya18012004@gmail.com>
2024-08-13 09:58:18 +0530

    BulbToggle feature

--- practice_react/src/App.jsx
+++ practice_react/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import WeatherCard from "./components/WeatherCard"
+import BulbToggle from './components/BulbToggle'
 const App = () => {
   return (
 <>
-<WeatherCard
+{/* <WeatherCard
 data ="40" 
 pic="https://media.istockphoto.com/id/510978989/photo/hawa-mahal-palace-in-jaipur-rajasthan.jpg?s=612x612&w=0&k=20&c=-2ijZ9kpofMH1jJhaxFF1hJ5oqjdXfOtIu0BlwXwdls="
 Location={"Jaipur"}
@@ -13,7 +14,9 @@ Location={"Jaipur"}
 data = "30" 
 pic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJoHP_uzR2BMYxEy6Rgr4RBgJ2m48SaChhkA&s"
 Location={"Agra"}
-/>
+/> */}
+
+<BulbToggle />
 </>
 
 
